9730c93d3bb651a4d3ec92d5170a2d5d7b876a8b
Jana Novotná <jana.posedelova@gmail.com>
2020-05-15 15:43:24 +0200

    Hotová povinná část cvičení

--- index.js
+++ index.js
@@ -6,14 +6,33 @@ const messagesElement = document.querySelector('#messages');
 
 const renderMessage = (name, message, date) => {
   // @TODO: funkce vracející HTML zprávy
+  return `
+ <div class="card mt-3 mb-3">
+					<div class="card-body">
+						<h5 class="card-title">
+							${name}
+							<small class="text-muted">${date}</small>
+						</h5>
+						<p class="card-text">${message}</p>
+					</div>
+				</div>
+ `
 };
 
 const renderMessages = (messages) => {
   // @TODO: funkce vypisující zprávy na stránku
+  messagesElement.innerHTML = '';
+
+  for (let i = 0; i < messages.length; i++) {
+    messagesElement.innerHTML += renderMessage(messages[i].name, messages[i].message, messages[i].date)
+  }
 };
 
 const updateMessages = () => {
   // @TODO: funkce stahující zprávy ze server a přidávající je na stránku
+  fetch('https://czechichat.herokuapp.com/api/list-messages')
+    .then(response => response.json())
+    .then(data => renderMessages(data.messages))
 };
 
 setInterval(updateMessages, 2000); // Každé dvě sekundy zavolá updateMessages
@@ -26,15 +45,26 @@ const messageInputElement = document.querySelector('#message-input');
 const onSubmit = (event) => {
   event.preventDefault(); // Zamezí přesměrování na jinou stránku při odesílání formuláře
 
-  console.log(
-    'Data:',
-    JSON.stringify({
+  // console.log(
+  //   'Data:',
+  //   JSON.stringify({
+  //     name: nameInputElement.value,
+  //     message: messageInputElement.value,
+  //   }),
+  // );  Jak vím, že v klíči body mám použít zrovna tohle??
+
+  // @TODO: odešli zprávu na server
+
+  fetch('https://czechichat.herokuapp.com/api/send-message', {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json'
+    },
+    body: JSON.stringify({
       name: nameInputElement.value,
       message: messageInputElement.value,
     }),
-  );
-
-  // @TODO: odešli zprávu na server
+  })
 };
 
 document.querySelector('#send-form').addEventListener('submit', onSubmit);
